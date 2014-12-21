447aadd8aa65f36c675bfdf4d1b7cdcf1a994630
Leslie Turner <a.leslieann@gmail.com>
2014-12-21 13:39:21 -0600

    Initialized Cards

--- main.rb
+++ main.rb
@@ -1,29 +1,43 @@
 require 'rubygems'
 require 'sinatra'
 
+
 set :sessions, true
 
-get '/new_game' do
+get '/' do
+  if session[:username]
+    redirect '/game'
+  else
+    redirect '/new_player'
+  end
+end
+
+get '/new_player' do
   erb :enter_name
 end
 
-post '/myaction' do
-  puts params['username']
+post '/new_player' do
+  session[:username] = params[:username]
+  redirect '/game'
 end
 
-get '/bet' do
-  erb :enter_bet
-end
 
-post '/postbet' do
-  puts params['userbet']
-end
 
 get '/game' do
-  erb :gameplay
+ 
+
+  suits = ['H', 'D', 'S', 'C']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
+
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+
+ erb :game
 end
 
-get 'game_over' do
-  erb :game_over
-end
 
